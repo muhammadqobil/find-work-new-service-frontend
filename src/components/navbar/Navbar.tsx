3dee1120ace023ba748a73b19bda934ea20f8fb0
Muhammadqobil <saidahmedovmuhammadqobil@gmail.com>
2025-04-04 14:42:20 +0500

    add

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Navbar = ({}): React.JSX.Element => {
     return (
         <div className={`flex justify-center items-center w-full h-[60px] shadow-md bg-white p-3 rounded-md`}>
-            <h1 className={"font-[500] leading-[1.75rem] text-gray-900 text-xl"}> ООО "Find Work New Service"</h1>
+            <h1 className={"font-[500] leading-[1.75rem] text-gray-900 text-xl"}> ООО &quot;Find Work New Service&quot;</h1>
         </div>
     )
 }
